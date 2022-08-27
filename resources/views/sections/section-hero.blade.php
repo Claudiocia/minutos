d036ebbe444a0b7e198be626e368e38263e8a166
Claudiocia <claudiosouza.cia@gmail.com>
2022-08-27 13:13:07 -0300

    acrescent teste aos3

--- resources/views/sections/section-hero.blade.php
+++ resources/views/sections/section-hero.blade.php
@@ -4,7 +4,7 @@
             <div class="col-12 hero-text-image">
                 <div class="row">
                     <div class="col-lg-5 text-center text-lg-start">
-                        <h1 data-aos="fade-right">Informação sem tempo a perder</h1>
+                        <h1 data-aos="zoom-in">Informação sem tempo a perder</h1>
 
                         <p>Vasculhamos mais de <strong class="minutos">90 fontes de notícias</strong> para te entregar os fatos mais
                             relevantes do dia num único e empolgante boletim informativo.</p>
@@ -15,13 +15,13 @@
                         <p>De segunda a sexta, bem cedinho em sua <strong class="minutos">caixa de e-mail.</strong> Assine agora a
                             nossa newsletter e se prepare para o dia em minutos. <strong class="minutos">É 100% de graça!</strong></p>
                         <div class="btn-hero">
-                            <p data-aos="fade-right" data-aos-delay="100" data-aos-offset="-500"><a href="{{route('clientes.index')}}" class="btn btn-assinar">ASSINE</a></p>
+                            <p data-aos="zoom-out" data-aos-delay="100"><a href="{{route('clientes.index')}}" class="btn btn-assinar">ASSINE</a></p>
                         </div>
                         <p class="font-texto-cancel">Não tem mais interesse? Em apenas dois cliques você pode cancelar.</p>
                     </div>
                     <div class="col-7 d-flex justify-content-center">
                         <div class="col-lg-7 iphone-wrap">
-                            <img src="{{asset('site/img/hero5.png')}}" alt="Image" class="phone-1" data-aos="fade-right">
+                            <img src="{{asset('site/img/hero5.png')}}" alt="Image" class="phone-1" data-aos-delay="200">
                         </div>
                     </div>
                 </div>
