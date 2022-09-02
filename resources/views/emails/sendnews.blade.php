80da896aa3ea199b1f57ea39bce766b89ac860a2
Claudiocia <claudiosouza.cia@gmail.com>
2022-09-02 08:58:42 -0300

    testando envio e formato de emails9

--- resources/views/emails/sendnews.blade.php
+++ resources/views/emails/sendnews.blade.php
@@ -17,10 +17,7 @@
     <div class="row desk-email">
     <table class="table-abert">
     <tr>
-    <td><h6>{{$mailNews['diaNews']}}</h6></td>
-    </tr>
-    <tr>
-    <td><h6 class="data-news">{{$mailNews['dataNews']}}</h6></td>
+    <td><h6>{{$mailNews['diaNews']." ".$mailNews['dataNews']}}</h6></td>
     </tr>
     <tr>
     <td><img src="{{asset('icones/minutos.png')}}" height="55" class="img-fluid"/></td>
@@ -100,7 +97,7 @@
     @foreach($mailNews['hist_dia'] as $noti)
     <a href="https://twitter.com/intent/tweet?text={{route('noticias.show', ['id' => $noti->retranca_id])}}" target="_blank"><img src="{{asset('icones/sociais/twitter.png')}}" alt="twitter" width="16" class="ico-soc-w img-fluid" /></a>
     <a href="https://www.facebook.com/sharer/sharer.php?u={{route('noticias.show', ['id' => $noti->retranca_id])}}" target="_blank"><img src="{{asset('icones/sociais/facebook.png')}}" alt="facebook" height="16" class="ico-soc-h img-fluid" /></a>
-    <a href="mailto:?subject={{route('noticias.show', ['id' => $noti->retranca_id])}}" target="_blank"><img src="{{asset('icones/sociais/envelope.png')}}" alt="email" width="16" class="ico-soc-w img-fluid" /></a>
+    <a href="mailto:?subject=Canal-Minutos?body={{route('noticias.show', ['id' => $noti->retranca_id])}}" target="_blank"><img src="{{asset('icones/sociais/envelope.png')}}" alt="email" width="16" class="ico-soc-w img-fluid" /></a>
     <a href="https://api.whatsapp.com/send?text={{route('noticias.show', ['id' => $noti->retranca_id])}}" target="_blank"><img src="{{asset('icones/sociais/whatsapp.png')}}" alt="whatsapp" width="16" class="ico-soc-w img-fluid" /></a>
     @break
     @endforeach
