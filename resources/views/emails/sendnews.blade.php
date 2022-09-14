7d88e123bff6108db95e487728b33b78432c75ad
Claudiocia <claudiosouza.cia@gmail.com>
2022-09-14 13:41:03 -0300

    Final newsletter 5

--- resources/views/emails/sendnews.blade.php
+++ resources/views/emails/sendnews.blade.php
@@ -19,7 +19,7 @@
     </tr>
     @if($mailNews['foto_parca'] != null)
     <tr>
-    <td style="font-size: 9px; text-align: center;"><p>Em parceria com:</p></td>
+    <td style="text-align: center;"><p style="font-size: 9px;">Em parceria com:</p></td>
     </tr>
     <tr>
     <td><img src="{{asset($mailNews['foto_parca'])}}" width="180" class="img-fluid"/></td>
