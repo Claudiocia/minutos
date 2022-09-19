c7dc52f62edf367c32852a3af84bcd66367b39af
Claudiocia <claudiosouza.cia@gmail.com>
2022-09-19 09:47:10 -0300

    Ajuste abertura

--- resources/views/sections/section-hero.blade.php
+++ resources/views/sections/section-hero.blade.php
@@ -3,7 +3,7 @@
         <div class="row align-items-center">
             <div class="col-12 hero-text-image">
                 <div class="row">
-                    <div class="col-lg-5 text-center text-lg-start">
+                    <div class="col-lg-6 text-center text-lg-start">
                         <h1>{{$site->title_site}}</h1>
                         {!! $site->text_abert !!}
                         @if($site->cancel_one != null)
@@ -14,7 +14,7 @@
                         </div>
                         <p class="font-texto-cancel">{{$site->cancel_two}}</p>
                     </div>
-                    <div class="col-7 d-flex justify-content-center">
+                    <div class="col-6 d-flex justify-content-center">
                         <div class="col-lg-7 iphone-wrap">
                             <img src="{{asset('site/img/hero5.png')}}" alt="Image" class="phone-1">
                         </div>
